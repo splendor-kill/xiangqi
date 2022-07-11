efde8c5d7b88ff359b2b50aecb712493c2ed3256
splendor-kill <splendor.kill@gmail.com>
2022-07-11 12:37:53 +0800

    export names

--- xiangqi/__init__.py
+++ xiangqi/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 import os
 import sys
@@ -8,3 +8,5 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from constants import Camp, Force, N_COLS, N_ROWS
 from board import get_iccs_action_space
 from env import Env
+
+__all__ = ['Env', 'Camp', 'Force', 'N_COLS', 'N_ROWS', 'get_iccs_action_space']
